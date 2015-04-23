b472dd5277c6d3c766b7c38a42aad3a37f6239cc
cocghe210 <cocghe210@gmail.com>
2015-04-23 20:33:27 +0900

    solution version 1.01

--- sf/Assignment06_08.v
+++ sf/Assignment06_08.v
@@ -55,8 +55,71 @@ Theorem pigeonhole_principle: forall (X:Type) (l1  l2:list X),
    length l2 < length l1 -> 
    repeats l1.  
 Proof.
-   intros X l1. induction l1 as [|x l1'].
+ intros X l1. induction l1 as [|x l1'].
+  induction l2. intros H1 H2 H3. inversion H3. intros H1 H2 H3. inversion H3.
+  intros l2 EM A L.
+  assert (repeats l1' \/ ~ repeats l1'). apply EM. induction H.
+  apply rep_step . apply H.
 
-   (* FILL IN HERE *) admit. admit.
+  assert ((appears_in x l1' \/ ~ appears_in x l1')). apply EM. inversion H0. apply rep_base. apply H1.
+apply rep_step. assert (appears_in x l2). apply A. apply ai_here.
+apply  appears_in_app_split in H2. inversion H2. inversion proof.
+apply IHl1' with (l2:=witness++ witness0). apply EM. intros.
+ assert(forall x0:X, x0 <> x -> appears_in x0 l1' -> appears_in x0 (witness++witness0)).
+intros. apply ai_later with (b :=x)in H5. apply A in H5. rewrite proof0 in H5.
+
+
+Lemma app_nil_right : forall (X:Type) (l : list X),
+  l ++ [] = l.
+Proof. intros. induction l as [|h t]. simpl. reflexivity. simpl. rewrite -> IHt. reflexivity.
 Qed.
 
+Lemma unapp : forall (X:Type) (x:X) (l1 l2 : list X),
+  x :: l1 ++ l2 = (x::l1) ++ l2.
+Proof. intros. simpl. reflexivity. Qed.
+
+Lemma appears_in_reorder : forall (X:Type) (x:X) (l1 l2 : list X),
+  appears_in x (l1++l2) -> appears_in x (l2++l1).
+Proof.
+  intros X x l1. induction l1 as [|h t].
+  Case "l=[]". intros. simpl in H. rewrite -> app_nil_right. apply H.
+  Case "l=h::t". intros. simpl in H. apply app_appears_in.
+  rewrite -> unapp in H. apply appears_in_app in H.
+  inversion H. right. apply H0. left. apply H0.
+Qed.
+
+
+apply appears_in_reorder in H5. simpl in H5. 
+
+Lemma appears_in_subl : forall (X:Type) (x  y :X) (xs : list X),
+  x <> y -> appears_in x (y::xs) -> appears_in x xs.
+Proof. intros. inversion H0. apply H in H2. inversion H2. apply H2. Qed.
+
+ apply appears_in_subl in H5. apply appears_in_reorder in H5. apply H5.
+intros. apply H4. apply H4. intro. rewrite H5 in H3.
+apply H1.  apply H3. apply H3.
+Lemma le_trans : forall m n o, m <= n -> n <= o -> m <= o.
+Proof.
+  intros m n o Lmn Lno.
+  generalize dependent Lmn.
+  generalize dependent m.
+  induction Lno.
+ intros. apply Lmn.
+ intros. apply le_S. apply IHLno. apply Lmn.
+Qed.
+
+Theorem n_le_Sn : forall n, n <= S n.
+Proof. intros. apply le_S. apply le_n. Qed.
+
+Theorem SnleSm : forall n m,
+  S n <= S m -> n <= m.
+Proof.
+  intros. inversion H.
+    apply le_n.
+    apply le_trans with (n := S n). apply n_le_Sn.
+    apply H1.
+Qed.
+
+rewrite proof0 in L. rewrite -> app_length with (l1:=witness) (l2:=x::witness0) in L.
+simpl in L. rewrite -> plus_comm in L. simpl in L. rewrite -> plus_comm in L. rewrite <-app_length in L. apply SnleSm in L.
+apply L. Qed.
